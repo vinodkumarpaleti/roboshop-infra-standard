7d7e32d46ee5e052a0eeb785452c1473ab0ffb51
vinodkumar <kumarvinod.paleti@gmail.com>
2024-01-05 11:41:13 +0530

    new files are added

--- 11-mysql/mysql.sh
+++ 11-mysql/mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/centos
-git clone https://github.com/vinodkumarpaleti/roboshop-shell.git
+git clone https://github.com/vinodkumarpaleti/roboshop-shell-tf.git
 cd roboshop-shell-tf
 sh mysql.sh

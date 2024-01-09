f5240a8153e5bdd5a8e7f89ec0c6bf14ca737a4f
vinodkumar <kumarvinod.paleti@gmail.com>
2024-01-09 10:44:18 +0530

    changes are done

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  #source = "git::https://github.com/vinodkumarpaleti/terraform-aws-vpc-advanced.git"
-  source = "../../terraform-aws-vpc-advanced"
+  source = "git::https://github.com/vinodkumarpaleti/terraform-aws-vpc-advanced.git"
   project_name = var.project_name #"roboshop" instead of hard coding we are keeping in variables.tf
   cidr_block = var.cidr_block
   common_tags = var.common_tags
+  env = var.env
   public_subnet_cidr = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
   database_subnet_cidr = var.database_subnet_cidr
